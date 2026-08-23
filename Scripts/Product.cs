cf177e0fea3ded5d255507267351b55085d8cd82
Jorge Felico <jorgefelico@gmail.com>
2026-08-23 18:07:40 -0400

    Workign

--- Scripts/Product.cs
+++ Scripts/Product.cs
@@ -7,8 +7,33 @@ public partial class Product : Node3D
     [Export] public int Damage;
     [Export] public float Mass;
 
+    private RigidBody3D Body;
+
     public override void _Ready()
     {
-       
+        Body = FindRigidBody(this);
+        if(Body != null && Mass > 0f)
+        {
+            GD.Print("Name: " + Body.Name);
+            Body.Mass = Mass;
+        }
+    }
+
+    private static RigidBody3D FindRigidBody(Node node)
+    {
+        if(node is RigidBody3D body)
+        {
+            return body;
+        }
+
+        foreach (Node child in node.GetChildren())
+        {
+            RigidBody3D found = FindRigidBody(child); 
+            if(found != null)
+            {
+                return found;
+            }
+        }
+        return null;
     }
 }

6ea82e590b8c73000468cbfd0ab0f57ffb7856b2
Jorge Felico <jorgefelico@gmail.com>
2026-08-23 17:37:01 -0400

    Imporvements

--- Scripts/Product.cs
+++ Scripts/Product.cs
@@ -1,20 +1,14 @@
 using Godot;
 
-[Tool]
-public partial class Product : Area3D
+public partial class Product : Node3D
 {
-    [Export] public ItemData Item;
+    [Export] public StringName DisplayName;
+    [Export] public int Price;
+    [Export] public int Damage;
+    [Export] public float Mass;
 
     public override void _Ready()
     {
-        MeshInstance3D meshInstance = GetNode<MeshInstance3D>("MeshInstance3D");
-        if (Item.Mesh == null)
-        {
-            GD.PushError(Item.DisplayName + " has no Mesh assigned");
-        }
-        else
-        {
-            meshInstance.Mesh = Item.Mesh;
-        }
+       
     }
 }

662f5897d4330a90690b9b79f8964cbb8045230f
Jorge Felico <jorgefelico@gmail.com>
2026-08-22 23:53:32 -0400

    Character controller movement

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -16,11 +16,11 @@ public partial class PlayerController : CharacterBody3D
 
     public override void _Ready()
     {
-        if(Head == null)
+        if (Head == null)
         {
             Head = GetNode<Node3D>("Head");
         }
-        if(Camera == null)
+        if (Camera == null)
         {
             Camera = GetNode<Camera3D>("Camera");
         }
@@ -30,20 +30,55 @@ public partial class PlayerController : CharacterBody3D
 
     public override void _UnhandledInput(InputEvent @event)
     {
-        if(@event.IsActionPressed("ui_cancel")) {
-            if(Input.MouseMode == Input.MouseModeEnum.Visible) GetTree().Quit();
+        if (@event.IsActionPressed("ui_cancel"))
+        {
+            if (Input.MouseMode == Input.MouseModeEnum.Visible) GetTree().Quit();
             Input.MouseMode = Input.MouseModeEnum.Visible;
         }
 
-        if(@event is InputEventMouseMotion motion && Input.MouseMode == Input.MouseModeEnum.Captured)
+        if (@event is InputEventMouseMotion motion && Input.MouseMode == Input.MouseModeEnum.Captured)
         {
-            if(motion.Relative.Length() > 500f) return;
-            
+            if (motion.Relative.Length() > 500f) return;
             Head.RotateY(-motion.Relative.X * Sensitivity);
             float pitch = Camera.Rotation.X - motion.Relative.Y * Sensitivity;
             pitch = Mathf.Clamp(pitch, -MaxPitch, MaxPitch);
             Camera.Rotation = new Vector3(pitch, Camera.Rotation.Y, Camera.Rotation.Z);
         }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (IsOnFloor())
+        {
+            Velocity = new Vector3(Velocity.X, -Gravity, Velocity.Z);
+        }
+        else
+        {
+            Velocity = new Vector3(Velocity.X, Velocity.Y - Gravity * (float)delta, Velocity.Z);
+        }
+
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
+        {
+            Velocity = new Vector3(Velocity.X, JumpVelocity, Velocity.Z);
+        }
+
+        Vector2 movementAxis = Input.GetVector("move_left", "move_right", "move_back", "move_forward");
+        Vector3 direction = new Vector3(movementAxis.X, 0, -movementAxis.Y);
+        Vector3 worldDir = Head.GlobalBasis * direction;
+        Vector3 target = worldDir * Speed;
+        float newX = Mathf.MoveToward(Velocity.X, target.X, Accel * (float)delta);
+        float newZ = Mathf.MoveToward(Velocity.Z, target.Z, Accel * (float)delta);
+        if (movementAxis != Vector2.Zero)
+        {
+            Velocity = new Vector3(newX, Velocity.Y, newZ);
+        }
+        else
+        {
+            newX = Mathf.MoveToward(Velocity.X, 0, Friction * (float)delta);
+            newZ = Mathf.MoveToward(Velocity.Z, 0, Friction * (float)delta);
+            Velocity = new Vector3(newX, Velocity.Y, newZ);
+        }
+        MoveAndSlide();
     }
 
 }
